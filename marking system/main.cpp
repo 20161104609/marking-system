2324d12d766520840ec5d31d7b52a9a85e13e30d
20161104609 <a20161104609@admindeimac-3.local>
2018-06-25 11:39:50 +0800

    Initial Commit

---
+++ marking system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  marking system
+//
+//  Created by 20161104609 on 18/6/25.
+//  Copyright © 2018年 20161104609. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
