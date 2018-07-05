f28ff7e6701d074e655309ffade3d9fb1a885886
20161104609 <a20161104609@admindeiMac-3.local>
2018-07-05 18:45:02 +0800

    换了类的方法

--- marking system/main.cpp
+++ marking system/main.cpp
@@ -6,286 +6,354 @@
 //  Copyright © 2018年 20161104609. All rights reserved.
 //
 
+//
+//  main.cpp
+//  marking
+//
+//  Created by 20161104609 on 18/7/5.
+//  Copyright © 2018年 20161104609. All rights reserved.
+//
+
+#include <cstdio>
+#include <cstring>
 #include <iostream>
-#include <string>
-#include <String>
-int n ;
 using namespace std;
 
-struct  marking
-{//参赛人员信息
-    string  name;
-    string  sex;
-    string  Class;
-    string  form;
-    string   tel;
-    int number;//bianhao
-    double  score[10];
-    double sum;
-    double aver;
-    
-    
-};
-struct referee
-{//裁判
-    string name;
-    string sex;
-    string tel;
-};
-
-void intput(marking *m)//参赛人员信息
+class student       // student 类为管理系统的一个节点
 {
-    int i;
-    int j;
-    int flag=1;
-    cout<<"请要插入参赛人员学号位置:"<<endl;
-    cin>>j;
-    struct  marking s;
-    for (i=0; i<n; ) {
-        if(m[i].number!=j)
-        {
-            i++;
-        }
-        else{
-            break;
-        }
+    friend class studentMessage;
+    student      *next;
+    char         name[30];
+    char         num[30];
+    char         Class[10];
+    char         form[10];
+    char         tel[10];
+    char         sex[10];
+    double       score[6];
+    double       averageScore;//平均成绩
+public:
+    student()
+    {
+        strcpy(name, "null");
+        strcpy(num, "null");
+        strcpy(Class, "null");
+        strcpy(form, "null");
+        strcpy(tel, "null");
+        strcpy(sex, "null");
+        averageScore=0;
+        for (int i = 0; i < 6; ++i) score[i] = 0;//初始化
     }
+    student(const student &a)
+    {
+        strcpy(name, a.name);
+        strcpy(num, a.num);
+        strcpy(Class, a.Class);
+        strcpy(form, a.form);
+        strcpy(tel, a.tel);
+        averageScore=a.averageScore;
+        for (int i = 0; i < 6; ++i)
+            score[i] = a.score[i];
+    }
+    student &input()
+    {
+        cout << "姓名：";  cin >> name;
+        cout << "性别：";  cin >> sex;
+        cout << "学号：";  cin >> num;
+        cout << "班级：";  cin >> Class;
+        cout << "组成：";  cin >> form;
+        cout << "电话：";  cin >> tel;
+        for (int i = 0; i < 5; ++i)
+        {
+            cout << "输入第" << (i + 1) << "门成绩:";
+            cin >> score[i];
+            score[5] += score[i];
+            
+        }//求平均函数
+        double max = score[0];
+        double min = score[0];
+        int maxi = 0;
+        int mini = 0;
+        int k;
+        for( k = 0; k < 5; k++) {
+            if( max < score[k]) {
+                max = score[k];
+                maxi = k;
+            }
+            if( min > score[k]) {
+                min = score[k];
+                mini = k;
+            }
+            averageScore += score[k];
+       }
+        averageScore=averageScore-max-min;
+        cout<<"最大值"<<max<<endl;
+        cout<<"最小值:"<<min<<endl;
+        averageScore /= 3;
+        return *this;
+    }
+    student* get_next()
+    {
+        return next;
+    }
+    
    
-    while(flag)
+    void print()
     {
-        cout<<"name:"<<endl;
-        cin>>m[i].name;
-        cout<<"number:"<<endl;
-        cin>>m[i].number;
-        cout<<"sec:"<<endl;
-        cin>>m[i].sex;
-        cout<<"class:"<<endl;
-        cin>>m[i].Class;
-        cout<<"form:"<<endl;
-        cin>>m[i].form;
-        cout<<"tel:"<<endl;
-        cin>>m[i].tel;
-        
-        cout<<"成绩1:"<<endl;
-        cin>>m[i].score[1];
-        cout<<"成绩2:"<<endl;
-        cin>>m[i].score[2];
-        cout<<"成绩3:"<<endl;
-        cin>>m[i].score[3];
-        cout<<"成绩4:"<<endl;
-        cin>>m[i].score[4];
-        cout<<"成绩5:"<<endl;
-        cin>>m[i].score[5];
-        //m[i].sum=m[i].score[1]+m[i].score[2]+m[i].score[3]+m[i].score[4]+m[i].score[5];
-        n++;
-        flag=0;
-        
+        printf("%8s %8s %8s %7s %8s %12s ", name, sex,num,Class,form,tel);
+        for (int i = 0; i < 6; ++i) printf(" %8.2f ", score[i]);
+        printf("%8.2lf",averageScore);
+        printf("\n");
     }
-}
-void  put(referee *r)//裁判信息
-{   int i;
-    cout<<"输入裁判信息:"<<endl;
-    for(i=0;i<5;i++)
-    {
-        cout<<"name:"<<endl;
-        cin>>r[i].name;
-        cout<<"sec:"<<endl;
-        cin>>r[i].sex;
-        cout<<"tel:"<<endl;
-        cin>>r[i].tel;
-    }
-    
-}
-
-void ave(marking *m , int n)//求平均成绩
-{
-    int i;
-    m[i].sum=0;
-    m[1].aver=0;
-    for ( i= 0; i < n; i ++)
-    {
-        double max = m[i].score[1];
-        double min = m[i].score[1];
-        for (int j = 1; j < 6; j ++)
-        {
-            if (m[i].score[j] > max)
-            {
-                max =m[i].score[j];
-            }
-            if (m[i].score[j] < min)
-            {
-                min = m[i].score[j];
-            }
-            m[i].sum+=m[i].score[j];
-        }
-        m[i].sum = m[i].sum - max - min;
-        //aver=m[i].sum/=3;
-        cout<<m[i].sum;
-        m[i].aver=m[i].sum/3;
-        cout<<"max:"<<max<<endl;
-        cout<<"min:"<<min<<endl;
-        cout<<"Calculate complete!\n";
-        cout<<m[i].aver;
-    }
-    //cout<<"Calculate complete!\n";
-    //1cout<<m[i].aver;
-    
-    
-}
-void change(marking *m)//修改函数
-{
-    int i;
-    int q;
-    cout<<"请输出要修改的号码:"<<endl;
-    cin>>q;
-    struct  marking s;
-    for (i=0; i<n; ) {
-        if(m[i].number!=q)
-        {
-            i++;
-        }
-        else{
-            break;
-        }
-    }
-    cout<<i<<endl;
-    cout<<"请输出入新的参赛人姓名："<<endl;
-    cin>>s.name;
-    m[i].name=s.name;
-    cout<<"请输出入新的参赛人性别："<<endl;
-    cin>>s.sex;
-    m[i].sex=s.sex;
-    cout<<"请输出入新的参赛人班级："<<endl;
-    cin>>s.Class;
-    m[i].Class=s.Class;
-    cout<<"请输出入新的参赛人电话号码："<<endl;
-    cin>>s.tel;
-    m[i].tel=s.tel;
-    cout<<"请输出入新的参赛人类别："<<endl;
-    cin>>s.form;
-    m[i].form=s.form;
-    cout<<"请输出入新的参赛人成绩1："<<endl;
-    cin>>s.score[1];
-    m[i].score[1]=s.score[1];
-    cin>>s.score[2];
-    cout<<"请输出入新的参赛人成绩2："<<endl;
-    m[i].score[2]=s.score[2];
-    cin>>s.score[3];
-    cout<<"请输出入新的参赛人成绩3："<<endl;
-    m[i].score[3]=s.score[3];
-    cout<<"请输出入新的参赛人成绩4："<<endl;
-    cin>>s.score[4];
-    m[i].score[4]=s.score[4];
-    cout<<"请输出入新的参赛人成绩5："<<endl;
-    cin>>s.score[5];
-    m[i].score[5]=s.score[5];
-    
-    
     
 
-}
-void del(marking *m)//删除函数
-{
-    int i;
-    //int h;
-    int q;
-    //struct marking k;
-    cout<<"请输出要删除的号码:"<<endl;
-    cin>>q;
-    for (i=1; i<=n; )
+    void Swap() // 交换当前结点和下一个
     {
-        if(m[i].number!=q)
+        char temp[30];
+        strcpy(temp, name);
+        strcpy(name, next->name);
+        strcpy(next->name, temp);
+        
+        strcpy(temp, sex);
+        strcpy(sex, next->sex);
+        strcpy(next->sex, temp);
+        
+        strcpy(temp, num);
+        strcpy(num, next->num);
+        strcpy(next->num, temp);
+        
+        strcpy(temp, Class);
+        strcpy(Class, next->Class);
+        strcpy(next->Class, temp);
+        
+        strcpy(temp, form);
+        strcpy(form, next->form);
+        strcpy(next->form, temp);
+        
+        strcpy(temp, tel);
+        strcpy(tel, next->tel);
+        strcpy(next->tel, temp);
+
+        for (int i = 0; i < 6; ++i)
         {
-            i++;
+            int tmp = score[i];
+            score[i] = next->score[i];
+            next->score[i] = tmp;
         }
-        else{
-    
-            m[i]=m[i+1];
+        for (int i=0;i<6; i++) {
+         int tmp=averageScore;
+           averageScore = next->averageScore;
+           next->averageScore = tmp;
             
         }
-    }n--;
-    cout<<"delete succeed!!!!";
-}
-void  output (marking *m )//输出参赛人员信息
-{  int i;
-    for(i=0;i<n;i++)
+        
+
+    }
+    
+};
+
+class studentMessage
+{
+protected:
+    student *first;
+    student *last;
+public:
+    studentMessage()
     {
-        cout<<"名字："<<m[i].name<<"性别："<<m[i].sex<<"班级："<<m[i].Class<<"项目："<<m[i].form<<"电话："<<m[i].tel<<"成绩1："<<m[i].score[1]<<"成绩2："<<m[i].score[2]<<"成绩3："<<m[i].score[3]<<"成绩4："<<m[i].score[4]<<"成绩5："<<m[i].score[5]<<endl;
+        first = last = new student;
+    }
+    ~studentMessage()
+    {
+        Clear();
+        delete first;
+    }
+    studentMessage &Append();           // 在链表尾部插入节点
+    student *Find(const char *x);       // 查找
+    void Query();                       // 查询
+    studentMessage &Delete();           // 删除查找结点
+    studentMessage &Clear();            // 删除所有结点
+    studentMessage &Sort();             // 按第i门课排序
+    void print();                       // 显示所有节点
+    void menu();                         // 显示菜单
+    
+};
+
+studentMessage &studentMessage::Append()
+{
+    student x;
+    x.input();
+    student *ptr = last;
+    *ptr = x;
+    last = new student;
+    ptr->next = last;
+    return *this;
+}
+
+studentMessage &studentMessage::Delete()
+{
+    char x[30];
+    cout << "输入要删除的信息（学号或姓名）\n";
+    cin >> x;
+    student *ptr = Find(x);
+    
+    if (ptr == last)
+    {
+        cout << "未找到相关信息\n";
+        return *this;
+    }
+    ptr->print();
+    cout << "是否删除此信息？（是输入1，否输入0）";
+    cin >> x;
+    if (*x == '1')
+    {
+        if (ptr == first)
+        {
+            first = first->next;
+            delete first;
+        }
+        else
+        {
+            student *temp = first;
+            while (temp->next != ptr) temp = temp->next;
+            temp->next = ptr->next;
+            delete ptr;
+        }
+        cout << "已删除\n";
+    }
+    else
+    {
+        cout << "已取消\n";
+    }
+    return *this;
+}
+
+studentMessage &studentMessage::Clear()
+{
+    char m [30];
+    cout << "将删除所有信息。确认请按Y";
+    cin >> m;
+    if (*m != 'Y' && *m  != 'y')
+    {
+        cout << "已取消\n";
+        return *this;
+    }
+    student *ptr = first;
+    while (ptr != last)
+    {
+        student *next = ptr->next;
+        delete ptr;
+        ptr = next;
+    }
+    first = last;
+    return *this;
+}
+
+void studentMessage::print()
+{
+    if (first == last)
+    {
+        cout << "暂无信息\n";
+        return ;
+    }
+    cout << "---------------------------------成绩列表--------------------------------------------------------------" << endl;
+    cout << "序号      姓名     性别     学号       班级      类别       电话号     成绩1     成绩2     成绩3     成绩4     成绩5       总成绩   平均成绩" << endl;
+    int no = 1;
+    student *ptr = first;
+    while (ptr != last)
+    {
+        printf("%3d:", no++);
+        ptr->print();
+        ptr = ptr->next;
     }
 }
-void menu()
+
+student *studentMessage::Find(const char *x)
 {
-    
-    
-    
-    
-    cout<<"                    | -----------------------------\n";
-    cout<<"                    |         评分系统              \n";
-    cout<<"                    | -----------------------------\n";
-    cout<<"                    |            菜单栏             \n";
-    cout<<"                    | -----------------------------\n";
-    cout<<"                    |       1---录入参赛人员信息       \n";
-    cout<<"                    |       2---录入裁判信息          \n";
-    cout<<"                    |       3---修改参赛人员信息       \n";
-    cout<<"                    |       4---排名参赛人员信息       \n";
-    cout<<"                    |       5---参赛人员的平均成绩     \n";
-    cout<<"                    |       6---显示才赛人员的信息     \n";
-    cout<<"                    |       7---删除参赛人员信息       \n";
-    cout<<"                    |       0---退出系统             \n";
-    cout<<"                    | ----------------------------- \n";
-    
-    
-   
-    
+    student *ptr = first;
+    while (ptr != last)
+    {
+        if (strcmp(ptr->name, x) == 0 || strcmp(ptr->num, x) == 0)
+            break;
+        ptr = ptr->next;
+    }
+    return ptr;
+}
+
+void studentMessage::Query()
+{
+    char x[30];
+    cout << "输入你要查询的信息(学号或姓名)";
+    cin >> x;
+    student *ptr = Find(x);
+    if (ptr == last)
+    {
+        cout << "没有查询到相关信息\n";
+    }
+    else
+    {
+        ptr->print();
+    }
+}
+
+// 冒泡排序
+studentMessage &studentMessage::Sort()
+{
+    int x;
+    cout << "输入你想按照哪门课的成绩来排序?((1-5)总成绩输入6):";
+    cin >> x;
+    x--;
+    student *ptr, *cnt = last;
+    while (cnt != first)
+    {
+        ptr = first;
+        while (ptr->next != cnt)
+        {
+            if (ptr->next->score[x] > ptr->score[x])
+            {
+                ptr->Swap();
+            }
+            ptr = ptr->next;
+        }
+        cnt = ptr;
+    }
+    return *this;
+}
+
+void studentMessage::menu()
+{
+    cout << "===============================\n";
+    cout << "      学生成绩管理系统\n\n";
+    cout << "      1.显示所有学生成绩\n";
+    cout << "      2.添加信息\n";
+    cout << "      3.查询信息\n";
+    cout << "      4.删除信息\n";
+    cout << "      5.成绩排序\n";
+    cout << "      6.删除所有信息\n";
+    cout << "      0.退出\n";
+    cout << "===============================\n";
     
     
 }
 
-void save(marking *m)
+int main()
 {
-  
-        int i;
-        FILE* fp;
-        fp = fopen("/Users/a20161104609/Desktop/markingsystem/marking／student.txt", "w");
-        if(fp == NULL)
-            printf("The file can't be opened!\n");
-        else
-        {
-            for(i=0; i<n; i++)
-            {
-                fprintf(fp,"%s %s %s %s %s %f %f %f %f %f",m[i].name.c_str(), m[i].sex.c_str(),m[i].Class.c_str(),m[i].form.c_str(), m[i].tel.c_str(),m[i].score[1],m[i].score[2],m[i].score[3],m[i].score[4],m[i].score[5]);
-            }
-            printf("\t\t\t 保存成功！！！d\n");
-            fclose(fp);
-            return;
-        }
-   
-}
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    struct  marking  m [100];
-    struct referee r[10];
-    menu();//运行主菜单
-    //load(L);//读取学生存入的数据
-    int s;
-    cout<<"请输入要用的功能："<<endl;
-    while(cin>>s)
+    studentMessage L;
+    int a ;
+    L.menu();
+    
+    while (cin>>a)
     {
-        switch(s)
+        switch(a)
         {
-            case 1: intput(m);break;
-            case 2: put(r);break;
-            case 3: change(m);break;
-                //case 4: sort(L);break;
-            case 5:ave(m,n); break;
-            case 6:output(m);break;
-            case 7:del(m);break;
-            case 0:
-                save(m);
-                exit(0);
-                break;
+            case 1: L.print(); break;
+            case 2: L.Append(); break;
+            case 3: L.Query(); break;
+            case 4: L.Delete(); break;
+            case 5: L.Sort(); break;
+            case 6: L.Clear(); break;
         }
-        menu();//循环一下菜单
     }
+    
+    
     return 0;
 }
+
